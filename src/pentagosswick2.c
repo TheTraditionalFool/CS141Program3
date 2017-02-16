c99965a1e451cb13e9c2daf73ba168eb20e591f3
Stephen <sswick2@uic.edu>
2017-02-16 15:09:57 -0600

    reuploaded the actual file

--- src/pentagosswick2.c
+++ src/pentagosswick2.c
@@ -1,11 +1,56 @@
-#include "pentagosswick2.c"
+#include <stdio.h>
+#include <stdlib.h>
 
-pentagosswick2::pentagosswick2()
-{
-    //ctor
-}
+#define COLUMN_NO 6
+#define ROW_NO 6
 
-pentagosswick2::~pentagosswick2()
-{
-    //dtor
+void printBoard(char pentagoBoard[][COLUMN_NO]){
+    int rowPosition;
+    int columnPosition;
+    char letter = 65;
+    printf("1   ");
+    for(columnPosition = 0; columnPosition<COLUMN_NO; columnPosition++){
+        printf("%d ", columnPosition+1);
+    }
+    printf("  2\n");
+    printf("    ");
+    for(columnPosition = 1; columnPosition<COLUMN_NO*2; columnPosition++){
+        printf("-");
+    }
+    printf("\n");
+    for(rowPosition = 0; rowPosition<ROW_NO; rowPosition++){
+        printf("%c:  ", letter);
+        for(columnPosition = 0; columnPosition<COLUMN_NO; columnPosition++){
+            printf("%c ", pentagoBoard[rowPosition][columnPosition]);
+        }
+        printf(" :%c\n", letter);
+        letter++;
+    }
+    printf("    ");
+    for(columnPosition = 1; columnPosition<COLUMN_NO*2; columnPosition++){
+        printf("-");
+    }
+    printf("\n");
+    printf("3   ");
+    for(columnPosition = 0; columnPosition<COLUMN_NO; columnPosition++){
+        printf("%d ", columnPosition+1);
+    }
+    printf("  4\n");
+
+    return;
+}//end printBoard function
+
+
+
+int main(void){
+    int rowPosition;
+    int columnPosition;
+    char pentagoBoard[ROW_NO][COLUMN_NO];
+    for(rowPosition = 0; rowPosition<ROW_NO; rowPosition++){//initialize 6x6 array with 36 dots
+        for(columnPosition = 0; columnPosition<COLUMN_NO; columnPosition++){
+            pentagoBoard[rowPosition][columnPosition] = 46;
+        }
+    }//end initializing
+    printBoard(pentagoBoard);
+    return 0;
 }

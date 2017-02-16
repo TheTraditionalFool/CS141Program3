5df48e945d02c240f6841dc3189e44ec8a46e346
Stephen <sswick2@uic.edu>
2017-02-16 13:30:33 -0600

    First Commit, Added program and README.txt

---
+++ README.txt
@@ -0,0 +1,6 @@
+CS141 Program 3
+Pentago
+By: Mike Avendano & Stephen Swick
+
+Program to emulate pentago the game
+
---
+++ src/pentagosswick2.c
@@ -0,0 +1,11 @@
+#include "pentagosswick2.c"
+
+pentagosswick2::pentagosswick2()
+{
+    //ctor
+}
+
+pentagosswick2::~pentagosswick2()
+{
+    //dtor
+}

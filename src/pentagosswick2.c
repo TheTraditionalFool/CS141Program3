bcde64981ea40696dbb04fc95321fa88ed1658bc
Stephen <sswick2@uic.edu>
2017-02-16 20:32:25 -0600

    Added a getMove function

--- src/pentagosswick2.c
+++ src/pentagosswick2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define COLUMN_NO 6
 #define ROW_NO 6
@@ -35,16 +36,34 @@ void printBoard(char pentagoBoard[][COLUMN_NO]){
     for(columnPosition = 0; columnPosition<COLUMN_NO; columnPosition++){
         printf("%d ", columnPosition+1);
     }
-    printf("  4\n");
+    printf("  4\n\n");
 
     return;
 }//end printBoard function
 
+void getUserMove(int turnNumber, char* rowInput, int* columnInput, int* quadrantInput, char* directionInput){
+    printf("%d. Enter row, column, quadrant, direction for X: ", turnNumber);
+    scanf(" %c", rowInput);
+    *rowInput = toupper(*rowInput);
+    if (*rowInput == 'X'){
+        return;
+    }
+    scanf(" %1d", columnInput);
+    scanf(" %1d", quadrantInput);
+    scanf(" %c", directionInput);
+    *directionInput = toupper(*directionInput);
+    return;
+}
 
 
 int main(void){
     int rowPosition;
     int columnPosition;
+    int turnNumber = 1;
+    char userRowInput;
+    int userColumnInput;
+    int userQuadrantInput;
+    char userDirectionInput;
     char pentagoBoard[ROW_NO][COLUMN_NO];
     for(rowPosition = 0; rowPosition<ROW_NO; rowPosition++){//initialize 6x6 array with 36 dots
         for(columnPosition = 0; columnPosition<COLUMN_NO; columnPosition++){
@@ -52,5 +71,6 @@ int main(void){
         }
     }//end initializing
     printBoard(pentagoBoard);
+    getUserMove(turnNumber, &userRowInput, &userColumnInput, &userQuadrantInput, &userDirectionInput);
     return 0;
 }
